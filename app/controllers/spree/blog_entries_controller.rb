1a7ff2270f7d3f054f35d99edd2de1c0e50eb43f
Bart Muzyka <muzykabartek@gmail.com>
2016-03-02 13:45:26 +0800

    Updated with new scope - published

--- app/controllers/spree/blog_entries_controller.rb
+++ app/controllers/spree/blog_entries_controller.rb
@@ -5,40 +5,40 @@ class Spree::BlogEntriesController < Spree::StoreController
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
 
   def index
-    @blog_entries = Spree::BlogEntry.visible.page(@pagination_page).per(@pagination_per_page)
+    @blog_entries = Spree::BlogEntry.published.page(@pagination_page).per(@pagination_per_page)
   end
 
   def show
-    if try_spree_current_user.try(:has_spree_role?, "admin")
+    if try_spree_current_user.try(:has_spree_role?, 'admin')
       @blog_entry = Spree::BlogEntry.find_by_permalink!(params[:slug])
     else
-      @blog_entry = Spree::BlogEntry.visible.find_by_permalink!(params[:slug])
+      @blog_entry = Spree::BlogEntry.published.find_by_permalink!(params[:slug])
     end
     @title = @blog_entry.title
   end
 
   def tag
-    @blog_entries = Spree::BlogEntry.visible.by_tag(params[:tag]).page(@pagination_page).per(@pagination_per_page)
+    @blog_entries = Spree::BlogEntry.published.by_tag(params[:tag]).page(@pagination_page).per(@pagination_per_page)
     @tag_name = params[:tag]
   end
 
   def category
-    @blog_entries = Spree::BlogEntry.visible.by_category(params[:category]).page(@pagination_page).per(@pagination_per_page)
+    @blog_entries = Spree::BlogEntry.published.by_category(params[:category]).page(@pagination_page).per(@pagination_per_page)
     @category_name = params[:category]
   end
 
   def archive
-    @blog_entries = Spree::BlogEntry.visible.by_date(params).page(@pagination_page).per(@pagination_per_page)
+    @blog_entries = Spree::BlogEntry.published.by_date(params).page(@pagination_page).per(@pagination_per_page)
   end
 
   def feed
-    @blog_entries = Spree::BlogEntry.visible.limit(20)
+    @blog_entries = Spree::BlogEntry.published.limit(20)
     render :layout => false
   end
 
   def author
     @author = Spree.user_class.where(:nickname => params[:author]).first
-    @blog_entries = Spree::BlogEntry.visible.by_author(@author).page(@pagination_page).per(@pagination_per_page)
+    @blog_entries = Spree::BlogEntry.published.by_author(@author).page(@pagination_page).per(@pagination_per_page)
   end
 
   private
